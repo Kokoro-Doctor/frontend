2e91695b58a1d499ca1ed0c4212fad99ed56b951
kokoro <kokoro.doctor.ai@gmail.com>
2025-10-01 17:18:30 +0530

    git tracking env-vars.js going forward

---
+++ env-vars.js
@@ -0,0 +1,3 @@
+export const API_URL = "https://olj5e5aejl.execute-api.ap-south-1.amazonaws.com/prod"
+
+export const webClientId = "569847732356-rl6pnkut18s91cvsfipcuhlkptpoj8fh.apps.googleusercontent.com";
